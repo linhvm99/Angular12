37f555cb0c95729c30f442182412897b47fb131f
linhvm99 <linhvm99@gmail.com>
2022-05-22 21:23:06 +0700

    Learn pipe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public name = 'Manh Linh';
+  public age = 20;
+  public fruits = ['Chuối', 'Nho'];
+  public authors = [
+    {
+      id: 1,
+      firstName: 'Flora',
+      lastName: 'Twell',
+      email: 'ftwell0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.180.237.33',
+      salary: 2200
+    },
+    {
+      id: 2,
+      firstName: 'Priscella',
+      lastName: 'Signe',
+      email: 'psigne1@berkeley.edu',
+      gender: 'Female',
+      ipAddress: '183.243.228.65',
+      salary: 1000
+    },
+    // more data
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log('Trái cây: ', this.fruits);
   }
 
+  public resetName(): void {
+    console.log('Reset name');
+    this.name = '';
+  }
 }
